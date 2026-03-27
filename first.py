47c9cc97a7deaec7a8b0a51da45b27f4d88625e1
vamsi399 <your.email@example.com>
2026-03-27 12:49:01 +0530

    second code

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+"""from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -35,4 +35,67 @@ else:
 
 
 input("Press Enter to close browser...")
+driver.close()"""
+
+
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+driver.implicitly_wait(10)
+
+
+driver.find_element(By.NAME, "username").send_keys("Admin")
+
+driver.find_element(By.NAME, "password").send_keys("admin123")
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+print("Login Done")
+
+
+#input("Complete verification manually and press Enter...")
+
+act_tilte = driver.title 
+
+exp_title = "OrangeHRM" 
+if act_tilte == exp_title: 
+    print("Login test passed") 
+else: 
+    print("Login test failed")
+
+
+
+input("Press Enter to close browser...")
 driver.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
